3740d685d64b9c2693fe74c13eb64c713ab19600
Dean-98543 <yangmingnjau@163.com>
2022-11-04 18:16:12 +0800

    Add xx information

--- 06.Decision Tree 决策树（统计学习方法II第5章）/inforTheory.py
+++ 06.Decision Tree 决策树（统计学习方法II第5章）/inforTheory.py
@@ -20,16 +20,28 @@ def cal_entropy(data, X):
     return sum(-entropy(a))
 
 
-def cal_inforGain(data, A, D):
+def cal_condiEntropy(data, X, Y):
+    """: 计算在已知随机变量X的分布的情况下，随机变量Y的信息熵是多少
+    data: pd.DataFrame格式
+    X：随机变量（已知分布）
+    Y：随机变量
     """
-    data：pd.DataFrame格式
-    X：随机变量X（条件已知）
-    Y：随机变量Y
-    """
-    pi = data[A].value_counts() / data.shape[0]
-    ei = data.groupby(A).apply(lambda xx:cal_entropy(xx, D))
+    pi = data[X].value_counts() / data[X].shape[0]
+    ei = data.groupby(X).apply(lambda xxx:cal_entropy(xxx, Y))
     condi_entropy = sum(pi*ei)
-    return cal_entropy(data, D) - condi_entropy
+    return condi_entropy
+
+
+def cal_inforGain(data, A, D):
+    """:
+    计算特征A对训练数据集D的信息增益g(D, A),
+    定义为  集合D的经验熵H(D)  与  在给定特征A的条件下D的经验条件熵H(D|A)  之差
+
+    data：pd.DataFrame格式
+    A：特征
+    D：训练数据集
+    """
+    return cal_entropy(data, D) - cal_condiEntropy(data, X=A, Y=D)
 
 
 def cal_inforGainRatio(data, A, D):
